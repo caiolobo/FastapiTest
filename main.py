1aef6d24b4b5b02f892da3592cb208fd063c9d12
caiolobo <caiolobo09@gmail.com>
2024-04-24 04:39:36 -0300

    Conhecendo o FASTAPI

---
+++ main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, HTTPException 
+import mysql.connector
+from pydantic import BaseModel
+
+# estrutura de conexão com o banco de dados
+app = FastAPI()
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    database='test',
+)
+
+cursor = db.cursor()
+
+class Item(BaseModel):
+    name_item: str
+
+
+# método GET para puxar as informações do banco para a API
+
+@app.get('/itens', tags=['itens'])
+
+def read_itens():
+    try:
+        cursor.execute("select * from itens")
+        itens = [{'id_item': row[0], 'name_item': row[1]} for row in cursor.fetchall()]
+        return itens
+
+    except:
+        raise HTTPException(status_code=500, detail='Database error')
+    
+
+# Método POST para inserir informações no banco da API
+
+@app.post('/itens', response_model=Item)
+def create_item(item: Item):
+    try:
+        query = 'INSERT INTO itens (name_item) VALUES (%s)'
+        cursor.execute(query, (item.name_item, ))
+        db.commit()
+        return {"name_item" : item.name_item}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Database error: {str(e)}')
+    
+# Método PUT para atualizar dados no banco através da API
+
+@app.put('/itens/{id_item}', response_model=Item)
+def update_item(id_item: int, item: Item):
+    try:
+        query = "UPDATE itens SET name_item = %s WHERE id_item = %s"
+        cursor.execute(query, (item.name_item, id_item))
+        db.commit()
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail='Item não encontrado')
+        return {'id' : id_item, 'name_item' : item.name_item}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Database error: {str(e)}')
+    
+# Método Delete para poder apagar dados do banco através da API
+
+@app.delete('/item/{id_item}')
+def delete_item(id_item: int):
+    try:
+        query = "DELETE FROM itens WHERE id_item = %s"
+        cursor.execute(query, (id_item, ))
+        db.commit()
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail='Item não encontrado')
+        return {'message': 'Item Deletado'}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Database error: {str(e)}')
